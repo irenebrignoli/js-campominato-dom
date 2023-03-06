e580e2dc0bbd8a58f69fbc452c808d46e1559aee
Irene Brignoli <ire.brignoli@gmail.com>
2023-03-06 13:08:16 +0100

    sistemando griglia

--- js/main.js
+++ js/main.js
@@ -1,172 +1,109 @@
+
 /*
-Bonus
+Consegna
+Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell’esercizio ma solo l’index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l’inizializzazione di git).
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+BONUS:
 Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-- con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-- con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-- con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
-Consigli del giorno:  :party_wizard:
+- difficoltà 1 ⇒ 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+- difficoltà 2 ⇒ 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+- difficoltà 3 ⇒ 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+Consigli del giorno: :party_wizard:
 Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
 Ad esempio:
 Di cosa ho bisogno per generare i numeri?
 Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
 Le validazioni e i controlli possiamo farli anche in un secondo momento.
+
 */
 
-
-
 const playDom = document.getElementById('play');
+const gridDom = document.getElementById('grid');
 
-const resetDom = document.getElementById('reset');
-
-const logoDom = document.getElementById('logoname');
-
-const gridDOm = document.getElementById('grid');
-
-const selectDom = document.getElementById('difficulty');
-const easyDom = document.getElementById('easy');
-const hardDom = document.getElementById('hard');
-const crazyDom = document.getElementById('crazy');
-
-
-
-//Creo i quadratini con cliclo for  
-
-playDom.addEventListener('click', //Evento click per la griglia di partenza 
+playDom.addEventListener('click', 
 
   function(){
-    gridDOm.classList.add('show');
-
-    if(selectDom.value == 1){ //difficoltà 1 => 100 caselle, divise in 10 caselle per 10 righe
-
-      for (let i = 1; i <= 100; i ++){
-      
-        const square = newSquareGenerator('square', 'easy');
-      
-        square.addEventListener('click',
-          function(){
-            this.classList.toggle('clicked');
-            console.log(i);
-          }
-        )
-      
-        gridDOm.append(square);
-      
-        square.innerHTML = `<div>${i}</div>`;
-      
-        playDom.addEventListener('click', //Reset gioco se riclicclo Play
-      
-          function(){
-            square.classList.remove('clicked');
-          }
-        )
+    gridDom.innerHTML = "";
+    let game = createNewGame();
+    gridDom.innerHTML = game;
     
+    /*
+    square.addEventListener('click',
+      function(){
+      this.classList.toggle('clicked');
+      console.log(i);
       }
-    
-    }else if(selectDom.value == 2){ //difficoltà 2 => 81 caselle,  9 caselle per 9 righe
-    
-      for (let i = 1; i <= 81; i ++){
-      
-        const square = newSquareGenerator('square', 'hard');
-      
-        square.addEventListener('click',
-          function(){
-            this.classList.toggle('clicked');
-            console.log(i);
-          }
-        )
-      
-        gridDOm.append(square);
-      
-        square.innerHTML = `<div>${i}</div>`;
-      
-        playDom.addEventListener('click', //Reset gioco se riclicclo Play
-      
-          function(){
-            square.classList.remove('clicked');
-          }
-        )
-      
-        resetDom.addEventListener('click',   //Reset gioco se clicco Reset
-      
-          function(){
-            square.classList.remove('clicked');
-          }
-        )
-      }
-    
-    }else if(selectDom.value == 3){ //con difficoltà 3 => 49 caselle,  in 7 caselle per 7 righe
-    
-      for (let i = 1; i <= 49; i ++){
-      
-        const square = newSquareGenerator('square', 'crazy');
-      
-        square.addEventListener('click',
-          function(){
-            this.classList.toggle('clicked');
-            console.log(i);
-          }
-        )
-      
-        gridDOm.append(square);
-      
-        square.innerHTML = `<div>${i}</div>`;
-      
-        playDom.addEventListener('click', //Reset gioco se riclicclo Play
-      
-          function(){
-            square.classList.remove('clicked');
-          }
-        )
-      
-        resetDom.addEventListener('click',   //Reset gioco se clicco Reset
-      
-          function(){
-            square.classList.remove('clicked');
-          }
-        )
-      }
-    }
+    )*/
   }
-)
 
-resetDom.addEventListener('click',   //Reset gioco se clicco Reset
-      
-  function(){
-    gridDOm.classList.remove('show');
+
+
+);
+
+
+
+
+
+
+
+
+
+
+
+
+//FUNCTIONS
+
+//do istruzioni in base alla difficoltà
+function createNewGame(){
+
+  const difficultyDom = document.getElementById('difficulty');
+  const difficulty = difficultyDom.value;
+
+  let squares;
+  let squaresRow;
+
+  switch (difficulty){
+
+    case "1":
+      squares: 100;
+      break;
+
+    case "2":
+      squares: 81;
+      break;
+    
+    case "3":
+      squares: 49;
+      break;
   }
-)
 
+  squaresRow = Math.sqrt(squares);
 
-logoDom.addEventListener('click', //Reset griglia
-  
-  function(){
-    gridDOm.classList.remove('show');
-  }
-)
+  createGameGrid(squares, squaresRow);
 
-
-
-
-
-
-
-//FUNZIONI
-
-// function removeClass(element, classe) {
-//   element.classList.remove(classe);
-//   return element;
-// }
-
-
-function newSquareGenerator(classe, classe2){
-  const element = document.createElement('div');
-  element.classList.add(classe);
-  element.classList.add(classe2);
-  return element;
 }
 
 
+//creo la grigllia usando una funzione che crea la singola cella
+function createGameGrid(squares, squaresRow){
+
+  for(let i = 1; 1 <= squares; i++){
+    generateGridSquare(squaresRow);
+  }
+}
 
 
+//creo la cella
+function generateGridSquare(squaresRow){
 
+  const square = document.createElement('div');
+  square.classList.add('square');
+  square.style.width = `calc(100% / ${squaresRow})`;
+  square.style.height = `calc(100% / ${squaresRow})`;
+  square.innerHTML = `<div>${i}</div>`;
 
+  return square;
+}
